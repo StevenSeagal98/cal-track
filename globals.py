081c1e8dfe18bc303b873afd9c16b068eee09b12
nateorona98 <nathaniel.orona98@gmail.com>
2024-07-20 14:47:48 -0500

    init

--- globals.py
+++
@@ -1,8 +0,0 @@
-class Globals:
-    def __init__(self):
-        self.preferences_file_path = ''
-        self.calorie_tracker_data_file_path = ''
-        self.preferences = {}
-        self.calorie_tracker_data = []
-
-globals = Globals()
